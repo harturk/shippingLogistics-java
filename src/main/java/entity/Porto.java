adde0922b4c33849253d5c6f5e28a88ce996c999
Hártur Kunzler <hartur.kunzler@gmail.com>
2023-05-25 20:25:45 -0300

    04 - Criação Classe Porto

---
+++ src/main/java/entity/Porto.java
@@ -0,0 +1,25 @@
+package entity;
+
+public class Porto {
+    private int id;
+    private String nome;
+    private String pais;
+
+    public Porto(int id, String nome, String pais) {
+        this.id = id;
+        this.nome = nome;
+        this.pais = pais;
+    }
+
+    public int getId() {
+        return this.id;
+    }
+
+    public String getNome() {
+        return this.nome;
+    }
+
+    public String getPais() {
+        return this.pais;
+    }
+}
