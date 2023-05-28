187bb4f5300cc39d5943cdb925a47da74b7ff0f5
VitorJacom <vjds.vitor@gmail.com>
2023-05-27 21:05:18 -0300

    Implementacao da ListaNavio +Consiste guardar um ArrayList de Navio e ja organizar em ordem de nome

---
+++ src/main/java/application/ListaNavios.java
@@ -0,0 +1,54 @@
+package application;
+
+import entity.Navio;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+
+public class ListaNavios {
+    private ArrayList<Navio> lista;
+
+    // inner class
+    private class NavioNomeComparator implements Comparator<Navio> {
+
+        @Override
+        public int compare(Navio navio1, Navio navio2) {
+            return navio1.getNome().compareTo(navio2.getNome());
+        }
+
+    }
+
+    public ListaNavios() {
+        lista = new ArrayList<Navio>();
+    }
+
+    public void cadastrarNavio(String nome, double velocidade, double autonomia, double custoMilhaPorMilhaBasico) {
+        Navio novoNavio = new Navio(nome, velocidade, autonomia, custoMilhaPorMilhaBasico);
+        if (igual(novoNavio)) {
+            System.err.println("Navio com o mesmo nome já foi cadastrado, o cadastro foi cancelado.");
+            return;
+        }
+        lista.add(novoNavio);
+
+        ordenaLista();
+
+    }
+
+    private boolean igual(Navio navio) {
+        String nomeNavio = navio.getNome().trim().toUpperCase();
+        for (Navio n : lista) {
+            String nomeN = n.getNome().trim().toUpperCase();
+            if (nomeN.equals(nomeNavio)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private void ordenaLista() {
+        NavioNomeComparator comparator = new NavioNomeComparator();
+        Collections.sort(lista, comparator);
+    }
+
+}
