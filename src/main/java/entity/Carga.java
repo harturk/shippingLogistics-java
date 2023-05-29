faf11b7584ab9618fc2f9ccf895e37bc69a05476
Hartur <hartur.kunzler@gmail.com>
2023-05-29 20:35:52 -0300

    Adiciona tipo de carga a classe Carga

--- src/main/java/entity/Carga.java
+++ src/main/java/entity/Carga.java
@@ -5,12 +5,14 @@ public class Carga {
     private int peso;
     private double valorDeclarado;
     private int tempoMaximo;
+    private TipoCarga tipoCarga;
 
-    public Carga(int indentificador, int peso, double valorDeclarado, int tempoMaximo) {
+    public Carga(int indentificador, int peso, double valorDeclarado, int tempoMaximo, TipoCarga tipoCarga) {
         this.indentificador = indentificador;
         this.peso = peso;
         this.valorDeclarado = valorDeclarado;
         this.tempoMaximo = tempoMaximo;
+        this.tipoCarga = tipoCarga;
     }
 
     public int getIndentificador() {
@@ -27,5 +29,9 @@ public class Carga {
 
     public int getTempoMaximo() {
         return this.tempoMaximo;
+    }
+
+    public TipoCarga getTipoCarga() {
+        return this.tipoCarga;
     }
 }
