c295bbfec3f23a3f0f718305e2ea66f7680cc5dc
Hartur Kunzler <hartur.kunzler@gmail.com>
2023-06-07 22:06:22 -0300

    Faz classe TipoCargas

--- src/main/java/entity/TipoCargaDuravel.java
+++ src/main/java/entity/TipoCargaDuravel.java
@@ -1,11 +1,11 @@
 package entity;
 
-public class CargaDuravel extends TipoCarga {
+public class TipoCargaDuravel extends TipoCarga {
     private String setor;
     private String material;
     private double ipi;
 
-    public CargaDuravel(int numero, String descricao, String setor, String material, double ipi) {
+    public TipoCargaDuravel(int numero, String descricao, String setor, String material, double ipi) {
         super(numero, descricao, 1.5);
         this.setor = setor;
         this.material = material;
