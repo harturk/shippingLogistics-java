14596d196200abc791f7caebd7c55bde58ce996e
VitorJacom <VJDS.vitor@gmail.com>
2023-06-18 01:12:50 -0300

    Leitura agr ler apropriatamente os arquivos

--- src/main/java/application/Leitura.java
+++ src/main/java/application/Leitura.java
@@ -3,8 +3,11 @@ package application;
 import java.io.IOException;
 import java.nio.charset.Charset;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Scanner;
+
+import model.*;
 
 /**
  * Classe de Leitura. Código trazido da aula, com um pequeno ajuste onde
@@ -13,39 +16,69 @@ import java.util.Scanner;
  */
 
 public class Leitura {
-    private String path;
-    private boolean hasHeader = false;
+    private String nomeArquivo;
+    private static Leitura leitura;
 
-    public Leitura(String path) {
-        this.path = path;
+    private Path cargas;
+    private Path clientes;
+    private Path distancia;
+    private Path navios;
+    private Path portos;
+    private Path tiposCargas;
+
+    private Leitura() {
     }
 
-    public Leitura(String path, boolean hasHeader) {
-        this.path = path;
-        this.hasHeader = hasHeader;
+    public static Leitura getLeitura(String path) {
+        if(leitura == null) {
+            leitura = new Leitura();
+        }
+        leitura.attPath(path);
+        
+        return leitura;
     }
 
-    /**
-     * Leitura dos dados dos objetos Pessoa de arquivo texto
-     * Alternativa 1: uso do Scanner token a token
-     */
-    public void leRegistrosTexto() {
-        String titulo;
-        String precoBase;
-        String tipo;
-        String categoriaDuracao;
-        try (Scanner sc = new Scanner(Files.newBufferedReader(Paths.get(this.path), Charset.defaultCharset()))) {
-            sc.useDelimiter("[;\\n]"); // separadores: ; e nova linha
-            if (this.hasHeader) {
-                sc.nextLine();
-            }
-            while (sc.hasNext()) {
-                titulo = sc.next().trim(); // Primeiro campo: titulo
-                precoBase = sc.next().trim(); // Segundo campo: preco base
-                tipo = sc.next().trim(); // Terceiro campo: tipo
-                categoriaDuracao = sc.next().trim(); // Quarto campo: categoria ou duracao
-                // Método para adicionar
-                // Mensagem de inserção ou não
+    private void attPath(String path){
+        this.nomeArquivo = path;
+        String cargasS = nomeArquivo+"-CARGAS.CSV";
+        String clientesS = nomeArquivo+"-CLIENTES.CSV";
+        String distanciaS = nomeArquivo+"-DISTANCIAS.CSV";
+        String naviosS = nomeArquivo+"-NAVIOS.CSV";
+        String portosS = nomeArquivo+"-PORTOS.CSV";
+        String tiposCargasS = nomeArquivo+"-TIPOSCARGAS.CSV";
+
+        this.cargas = Paths.get("../programacao-orientada-objetos-trab-final", "src", "Arquivos", cargasS);
+        this.clientes = Paths.get("../programacao-orientada-objetos-trab-final", "src", "Arquivos", clientesS);
+        this.distancia = Paths.get("../programacao-orientada-objetos-trab-final", "src", "Arquivos", distanciaS);
+        this.navios = Paths.get("../programacao-orientada-objetos-trab-final", "src", "Arquivos", naviosS);
+        this.portos = Paths.get("../programacao-orientada-objetos-trab-final", "src", "Arquivos", portosS);
+        this.tiposCargas = Paths.get("../programacao-orientada-objetos-trab-final", "src", "Arquivos", tiposCargasS);
+
+
+    }
+
+    public void carregarTudo(){
+        carregarClientes();
+        carregarDistancia();
+        carregarNavios();
+        carregarPortos();
+    }
+
+
+
+
+    private void carregarClientes(){
+        ListaClientes a = ListaClientes.listaClientes();
+        try (Scanner sc = new Scanner(Files.newBufferedReader(clientes, Charset.defaultCharset()))) {
+            sc.useDelimiter(";"); // separadores: ; e nova linha
+            sc.nextLine();
+            while(sc.hasNextLine()){
+                // cod;nome;email
+                int cod = Integer.parseInt(sc.next());
+                String nome = sc.next();
+                String email = sc.nextLine().substring(1);
+                a.cadastrarCliente(cod, nome, email);
+                
             }
         } catch (IOException e1) {
             System.err.format("Erro de E/S: %s%n", e1);
@@ -55,4 +88,100 @@ public class Leitura {
             e2.printStackTrace();
         }
     }
+
+    private void carregarDistancia(){
+        ListaDistancia a = ListaDistancia.getInstance();
+        try (Scanner sc = new Scanner(Files.newBufferedReader(distancia, Charset.defaultCharset()))) {
+            sc.useDelimiter(";"); // separadores: ; e nova linha
+            sc.nextLine();
+            while(sc.hasNextLine()){
+                int origem = Integer.parseInt(sc.next());
+                int destino = Integer.parseInt(sc.next());
+                String distanciaS = sc.nextLine().substring(1).replace(",", ".");
+                double distancia = Double.parseDouble(distanciaS);
+                a.cadastrarDistancia(origem, destino, distancia);
+                
+            }
+        } catch (IOException e1) {
+            System.err.format("Erro de E/S: %s%n", e1);
+        } catch (Exception e2) {
+            System.out.println("Erro: " + e2);
+            System.out.print("Erro - trace da falha: ");
+            e2.printStackTrace();
+        }
+    }
+
+    private void carregarNavios(){
+        ListaNavios a = ListaNavios.listaNavios();
+        try (Scanner sc = new Scanner(Files.newBufferedReader(navios, Charset.defaultCharset()))) {
+            sc.useDelimiter(";"); // separadores: ; e nova linha
+            sc.nextLine();
+            while(sc.hasNextLine()){
+                String nome = sc.next();
+                String velocidadeString = sc.next().replace(",", ".");
+                String autonomiaString = sc.next().replace(",", ".");
+                String custoMilhaString = sc.nextLine().substring(1).replace(",", ".");
+                double velocidade = Double.parseDouble(velocidadeString);
+                double autonomia = Double.parseDouble(autonomiaString);
+                double custoMilha = Double.parseDouble(custoMilhaString);
+
+                a.cadastrarNavio(nome, velocidade, autonomia, custoMilha);
+                
+            }
+        } catch (IOException e1) {
+            System.err.format("Erro de E/S: %s%n", e1);
+        } catch (Exception e2) {
+            System.out.println("Erro: " + e2);
+            System.out.print("Erro - trace da falha: ");
+            e2.printStackTrace();
+        }
+    }
+
+    private void carregarPortos(){
+        ListaPortos a = ListaPortos.listaPortos();
+        try (Scanner sc = new Scanner(Files.newBufferedReader(portos, Charset.defaultCharset()))) {
+            sc.useDelimiter(";"); // separadores: ; e nova linha
+            sc.nextLine();
+            while(sc.hasNextLine()){
+                //id;nome;pais
+                int id = Integer.parseInt(sc.next());
+                String nome = sc.next();
+                String pais = sc.nextLine().substring(1);
+                a.cadastrarPorto(id, nome, pais);                
+            }
+        } catch (IOException e1) {
+            System.err.format("Erro de E/S: %s%n", e1);
+        } catch (Exception e2) {
+            System.out.println("Erro: " + e2);
+            System.out.print("Erro - trace da falha: ");
+            e2.printStackTrace();
+        }
+    }
+
+        // public void carregarCargas(){
+    //     try (Scanner sc = new Scanner(Files.newBufferedReader(cargas, Charset.defaultCharset()))) {
+    //         sc.useDelimiter(";"); // separadores: ; e nova linha
+    //         sc.nextLine();
+    //         while(sc.hasNextLine()){
+    //             // codigo;cliente;origem;destino;peso;valordeclarado;tempomaximo;tipocarga;prioridade;situacao
+
+    //             int codigo = Integer.parseInt(sc.next());
+    //             String cliente = sc.next();
+    //             String origem = sc.next();
+    //             String destino = sc.next();
+    //             int peso = Integer.parseInt(sc.next());
+    //             String valorS = sc.next().replace(",", ".");
+    //             double valor = Double.parseDouble(valorS);
+
+
+    
+    //         }
+    //     } catch (IOException e1) {
+    //         System.err.format("Erro de E/S: %s%n", e1);
+    //     } catch (Exception e2) {
+    //         System.out.println("Erro: " + e2);
+    //         System.out.print("Erro - trace da falha: ");
+    //         e2.printStackTrace();
+    //     }
+    // }
 }
